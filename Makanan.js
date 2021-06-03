cc431b90d37314de7270cb206c8ec9f55dc160dd
rahadis <84191920+rahadis@users.noreply.github.com>
2021-06-03 12:49:09 +0700

    Add files via upload

---
+++ Makanan.js
@@ -0,0 +1,97 @@
+function filter()
+    {
+        if (document.getElementById("murah").checked == true)
+      {
+        a = document.getElementById("murah1").style.display = 'block';
+        b = document.getElementById("murah2").style.display = 'block';
+        c = document.getElementById("murah3").style.display = 'block';
+        d = document.getElementById("murah4").style.display = 'block';
+        e = document.getElementById("murah5").style.display = 'block';
+        f = document.getElementById("murah6").style.display = 'block';
+        g = document.getElementById("murah7").style.display = 'block';
+        h = document.getElementById("murah8").style.display = 'block';
+        i = document.getElementById("murah9").style.display = 'block';
+        j = document.getElementById("murah10").style.display = 'block';
+        k = document.getElementById("medium1").style.display = 'none';
+        l = document.getElementById("medium2").style.display = 'none';
+        m = document.getElementById("medium3").style.display = 'none';
+        n = document.getElementById("medium4").style.display = 'none';
+        o = document.getElementById("medium5").style.display = 'none';
+        p = document.getElementById("medium6").style.display = 'none';
+        q = document.getElementById("mahal1").style.display = 'none';
+        r = document.getElementById("mahal2").style.display = 'none';
+        s = document.getElementById("mahal3").style.display = 'none';
+        t = document.getElementById("mahal4").style.display = 'none';
+
+      }
+      else if (document.getElementById("medium").checked == true)
+      {
+        a = document.getElementById("murah1").style.display = 'none';
+        b = document.getElementById("murah2").style.display = 'none';
+        c = document.getElementById("murah3").style.display = 'none';
+        d = document.getElementById("murah4").style.display = 'none';
+        e = document.getElementById("murah5").style.display = 'none';
+        f = document.getElementById("murah6").style.display = 'none';
+        g = document.getElementById("murah7").style.display = 'none';
+        h = document.getElementById("murah8").style.display = 'none';
+        i = document.getElementById("murah9").style.display = 'none';
+        j = document.getElementById("murah10").style.display = 'none';
+        k = document.getElementById("medium1").style.display = 'block';
+        l = document.getElementById("medium2").style.display = 'block';
+        m = document.getElementById("medium3").style.display = 'block';
+        n = document.getElementById("medium4").style.display = 'block';
+        o = document.getElementById("medium5").style.display = 'block';
+        p = document.getElementById("medium6").style.display = 'block';
+        q = document.getElementById("mahal1").style.display = 'none';
+        r = document.getElementById("mahal2").style.display = 'none';
+        s = document.getElementById("mahal3").style.display = 'none';
+        t = document.getElementById("mahal4").style.display = 'none';
+
+      }
+      else if (document.getElementById("mahal").checked == true)
+      {
+        a = document.getElementById("murah1").style.display = 'none';
+        b = document.getElementById("murah2").style.display = 'none';
+        c = document.getElementById("murah3").style.display = 'none';
+        d = document.getElementById("murah4").style.display = 'none';
+        e = document.getElementById("murah5").style.display = 'none';
+        f = document.getElementById("murah6").style.display = 'none';
+        g = document.getElementById("murah7").style.display = 'none';
+        h = document.getElementById("murah8").style.display = 'none';
+        i = document.getElementById("murah9").style.display = 'none';
+        j = document.getElementById("murah10").style.display = 'none';
+        k = document.getElementById("medium1").style.display = 'none';
+        l = document.getElementById("medium2").style.display = 'none';
+        m = document.getElementById("medium3").style.display = 'none';
+        n = document.getElementById("medium4").style.display = 'none';
+        o = document.getElementById("medium5").style.display = 'none';
+        p = document.getElementById("medium6").style.display = 'none';
+        q = document.getElementById("mahal1").style.display = 'block';
+        r = document.getElementById("mahal2").style.display = 'block';
+        s = document.getElementById("mahal3").style.display = 'block';
+        t = document.getElementById("mahal4").style.display = 'block';
+      }
+      else
+      {
+        a = document.getElementById("murah1").style.display = 'block';
+        b = document.getElementById("murah2").style.display = 'block';
+        c = document.getElementById("murah3").style.display = 'block';
+        d = document.getElementById("murah4").style.display = 'block';
+        e = document.getElementById("murah5").style.display = 'block';
+        f = document.getElementById("murah6").style.display = 'block';
+        g = document.getElementById("murah7").style.display = 'block';
+        h = document.getElementById("murah8").style.display = 'block';
+        i = document.getElementById("murah9").style.display = 'block';
+        j = document.getElementById("murah10").style.display = 'block';
+        k = document.getElementById("medium1").style.display = 'block';
+        l = document.getElementById("medium2").style.display = 'block';
+        m = document.getElementById("medium3").style.display = 'block';
+        n = document.getElementById("medium4").style.display = 'block';
+        o = document.getElementById("medium5").style.display = 'block';
+        p = document.getElementById("medium6").style.display = 'block';
+        q = document.getElementById("mahal1").style.display = 'block';
+        r = document.getElementById("mahal2").style.display = 'block';
+        s = document.getElementById("mahal3").style.display = 'block';
+        t = document.getElementById("mahal4").style.display = 'block';
+      }
+    }
